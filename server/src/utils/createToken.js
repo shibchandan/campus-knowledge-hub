81c70122dc278c299c549265bd4764eaadcb9ed5
Shib Chandan Mistry <shibchandan11@gmail.com>
2026-08-06 17:41:24 +0530

    fix(server): inject jti into refresh tokens to prevent mongodb E11000 dup key crash during parallel refresh requests

--- server/src/utils/createToken.js
+++ server/src/utils/createToken.js
@@ -1,10 +1,12 @@
 import jwt from "jsonwebtoken";
 import { env } from "../config/env.js";
+import crypto from "crypto";
 
 export function createToken(payload) {
   return jwt.sign(payload, env.jwtSecret, { expiresIn: env.jwtExpiresIn });
 }
 
 export function createRefreshToken(payload) {
-  return jwt.sign(payload, env.jwtRefreshSecret, { expiresIn: env.jwtRefreshExpiresIn });
+  const payloadWithJti = { ...payload, jti: crypto.randomUUID() };
+  return jwt.sign(payloadWithJti, env.jwtRefreshSecret, { expiresIn: env.jwtRefreshExpiresIn });
 }
